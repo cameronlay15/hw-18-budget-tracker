925edb1a374efb0ac50df7c445e4d65e39d4be67
Cameron Lay <cameronlay15@gmail.com>
2020-10-26 20:55:53 -0400

    on success

---
+++ public/db.js
@@ -0,0 +1,15 @@
+let db;
+
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+  db = event.target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
