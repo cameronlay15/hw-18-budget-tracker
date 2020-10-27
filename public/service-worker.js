ddb7eb8e96cf38068f195be2cd5b646cc5197532
Cameron Lay <cameronlay15@gmail.com>
2020-10-26 21:22:29 -0400

    cache

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/styles.css',
+    '/index.js',
+    '/index.html',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Cached");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
